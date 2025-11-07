0c05efce80c98ed7d09d146d5239b8966455bb4c
vladimer <vladimer177@yahoo.com>
2025-11-07 15:49:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: './', // 👈 This line is the fix
+  plugins: [react(), tailwindcss()],
+  base: './', // ✅ important for proper asset paths
 })
